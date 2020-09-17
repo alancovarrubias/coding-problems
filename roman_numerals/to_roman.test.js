faea2eed4bb7af1ba8b3fbde778f67de9acd8ff5
Alan Covarrubias <alan.cov@icloud.com>
2020-09-17 02:22:18 -0700

    refactor using digits instead of string

--- roman_numerals/to_roman.test.js
+++ roman_numerals/to_roman.test.js
@@ -13,121 +13,70 @@
 //  thus IV = 4, XL = 40, CM = 900.
 //
 
-const buildVString = (n) => {
-  let iString = new Array(n + 1).join("I");
-  let vString = "";
-  while (iString.length >= 4) {
-    if (iString.length === 4) {
-      vString += "IV";
-      iString = iString.substring(4);
-    } else {
-      vString += "V";
-      iString = iString.substring(5);
-    }
-  }
-  return vString + iString;
+const digitValue = {
+  1: "I",
+  10: "X",
+  100: "C",
 };
 
-const buildXString = (n) => {
-  let vString = buildVString(n);
-  let xString = "";
-  let vMatches = vString.match(/V/g);
-  while (vMatches && vMatches.length >= 2) {
-    if (vString.substring(0, 2) === "VV") {
-      xString += "X";
-      vString = vString.substring(2);
-    } else {
-      xString += "IX";
-      vString = vString.substring(3);
-    }
-    vMatches = vString.match(/V/g);
-  }
-  return xString + vString;
+const firstHalfMapping = {
+  1: "IV",
+  10: "XL",
+  100: "CD",
+};
+const secondHalfMapping = {
+  1: "IX",
+  10: "XC",
+  100: "CM",
 };
 
-const buildLString = (n) => {
-  let xString = buildXString(n);
-  let lString = "";
-  while (xString.startsWith("XXXX")) {
-    if (xString.startsWith("XXXXX")) {
-      lString += "L";
-      xString = xString.substring(5);
-    } else {
-      lString += "XL";
-      xString = xString.substring(4);
-    }
-  }
-  return lString + xString;
+const secondHalfPrefixMapping = {
+  1: "V",
+  10: "L",
+  100: "D",
 };
-const buildCString = (n) => {
-  let lString = buildLString(n);
-  let cString = "";
-  let lMatches = lString.match(/L/g);
-  while (lMatches && lMatches.length >= 2) {
-    if (lString.substring(0, 2) === "LL") {
-      cString += "C";
-      lString = lString.substring(2);
-    } else {
-      cString += "XC";
-      lString = lString.substring(3);
-    }
-    lMatches = lString.match(/L/g);
-  }
-  return cString + lString;
+const charStringOfLength = (char, length) => {
+  return new Array(length + 1).join(char);
 };
-
-const buildDString = (n) => {
-  let cString = buildCString(n);
-  let dString = "";
-  while (cString.startsWith("CCCC")) {
-    if (cString.substring(0, 5) === "CCCCC") {
-      dString += "D";
-      cString = cString.substring(5);
-    } else {
-      dString += "CD";
-      cString = cString.substring(4);
-    }
+const divisorString = (n, divisor) => {
+  const digit = Math.floor(n / divisor) % 10;
+  const firstHalf = Math.floor(digit / 5) == 0;
+  const halfRemainder = digit % 5;
+  if (halfRemainder === 4) {
+    return firstHalf ? firstHalfMapping[divisor] : secondHalfMapping[divisor];
+  } else {
+    const prefix = firstHalf ? "" : secondHalfPrefixMapping[divisor];
+    return prefix + charStringOfLength(digitValue[divisor], halfRemainder);
   }
-  return dString + cString;
 };
-
+const thousandsString = (n) => {
+  const thousandsDigit = Math.floor(n / 1000) % 10;
+  return charStringOfLength("M", thousandsDigit);
+};
 const toRoman = (n) => {
-  let dString = buildDString(n);
-  let mString = "";
-  let dMatches = dString.match(/D/g);
-  while (dMatches && dMatches.length >= 2) {
-    if (dString.substring(0, 2) === "DD") {
-      mString += "M";
-      dString = dString.substring(2);
-    } else {
-      mString += "CM";
-      dString = dString.substring(3);
-    }
-    dMatches = dString.match(/D/g);
-  }
-  return mString + dString;
+  return (
+    thousandsString(n) +
+    divisorString(n, 100) +
+    divisorString(n, 10) +
+    divisorString(n, 1)
+  );
 };
-
 describe("toRoman", () => {
-  test("1-3", () => {
+  test("1-9", () => {
     expect(toRoman(1)).toBe("I");
     expect(toRoman(3)).toBe("III");
-  });
-  test("4-8", () => {
     expect(toRoman(4)).toBe("IV");
     expect(toRoman(5)).toBe("V");
     expect(toRoman(8)).toBe("VIII");
-  });
-  test("9 - 39", () => {
     expect(toRoman(9)).toBe("IX");
+  });
+  test("10 - 90", () => {
     expect(toRoman(10)).toBe("X");
     expect(toRoman(13)).toBe("XIII");
     expect(toRoman(15)).toBe("XV");
     expect(toRoman(20)).toBe("XX");
     expect(toRoman(38)).toBe("XXXVIII");
     expect(toRoman(39)).toBe("XXXIX");
-  });
-  test("40 - 89", () => {
     expect(toRoman(40)).toBe("XL");
     expect(toRoman(45)).toBe("XLV");
     expect(toRoman(48)).toBe("XLVIII");
@@ -135,22 +84,22 @@ describe("toRoman", () => {
     expect(toRoman(50)).toBe("L");
     expect(toRoman(88)).toBe("LXXXVIII");
     expect(toRoman(89)).toBe("LXXXIX");
-  });
-  test("90 - 399", () => {
     expect(toRoman(90)).toBe("XC");
+  });
+  test("100 - 999", () => {
     expect(toRoman(100)).toBe("C");
     expect(toRoman(388)).toBe("CCCLXXXVIII");
     expect(toRoman(389)).toBe("CCCLXXXIX");
     expect(toRoman(399)).toBe("CCCXCIX");
-  });
-  test("400 - 899", () => {
     expect(toRoman(400)).toBe("CD");
     expect(toRoman(500)).toBe("D");
     expect(toRoman(899)).toBe("DCCCXCIX");
-  });
-  test("900 - 1000", () => {
     expect(toRoman(900)).toBe("CM");
     expect(toRoman(950)).toBe("CML");
+    expect(toRoman(999)).toBe("CMXCIX");
+  });
+  test("1000 - 3000", () => {
     expect(toRoman(1000)).toBe("M");
+    expect(toRoman(3000)).toBe("MMM");
   });
 });
